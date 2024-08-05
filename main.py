c67cd59b4fc6c4c5681bf3de94d94100c04c12ca
paulrainy <science0instant0issue@gmail.com>
2024-08-06 00:05:12 +0300

    cluster df

--- main.py
+++ main.py
@@ -2,31 +2,43 @@ import pandas as pd
 import numpy as np
 import torch
 import math
+from pandas import DataFrame
 from torch import Tensor
 import torch.nn.functional as F
 from typing import List
 from transformers import AutoTokenizer, AutoModel
+from pymilvus import MilvusClient, DataType
+
 
 
 class EmbeddingPipeline:
-    def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
-        self.df = self.import_df(self.file_path)
-        self.processed_data = self.prepare_df()
+    file_path = None
+    df = None
+    prepared_df = None
+    processed_data = None
 
-    def get_file_path(self) -> str:
+    def __init__(self, file_path: str) -> None:  # конструктор
+        self.file_path = file_path
+
+    def get_file_path(self) -> str:  # геттер пути файла
         return self.file_path
 
-    @staticmethod
-    def import_df(path: str) -> pd.DataFrame:
+    def get_df(self) -> pd.DataFrame:
+        return self.df
+
+    def get_prepared_df(self) -> List[DataFrame]:
+        return self.prepared_df
+
+    def get_processed_data(self) -> List[Tensor]:
+        return self.processed_data
+
+    def import_df(self) -> None:
         try:
-            return pd.read_excel(path)
+            self.df = pd.read_excel(self.file_path)
         except Exception as e:
             raise ValueError(f"Error importing file: {e}")
 
-    def prepare_df(self) -> List[pd.DataFrame]:
-        df = self.import_df(self.file_path)
-
+    def prepare_df(self) -> None:
         def remove_columns_step(dataset: pd.DataFrame) -> pd.DataFrame:
             output_dataset = dataset[
                 ["Id", "Direction", "Section", "TestCaseName", "Preconditions", "Steps", "Postconditions",
@@ -35,7 +47,7 @@ class EmbeddingPipeline:
                 ["Id", "Direction", "Section", "TestCaseName"]].ffill()
             return output_dataset
 
-        output_first_step = remove_columns_step(df)
+        output_first_step = remove_columns_step(self.df)
 
         def parse_tests_by_id(dataset: pd.DataFrame) -> List[pd.DataFrame]:
             test_cases = []
@@ -68,10 +80,9 @@ class EmbeddingPipeline:
             return array
 
         output_third_step = remove_empty_cells(output_second_step)
+        self.prepared_df = output_third_step
 
-        return output_third_step
-
-    def process_df(self, df) -> List[Tensor]:
+    def process_df(self) -> None:
         if torch.cuda.is_available():
             device = torch.device('cuda')
         else:
@@ -89,7 +100,7 @@ class EmbeddingPipeline:
 
         model.to(device)
 
-        for test_case in df:
+        for test_case in self.prepared_df:
             # Tokenize
             encoded_input = tokenizer(str(test_case), return_tensors='pt', padding=True, truncation=True,
                                       max_length=512).to(device)
@@ -101,4 +112,27 @@ class EmbeddingPipeline:
             embedding = mean_pooling(model_output, encoded_input['attention_mask'])
             mpnet_vectorized_output_array.append(F.normalize(embedding, p=2, dim=1))
 
-        return mpnet_vectorized_output_array
+        self.processed_data = mpnet_vectorized_output_array
+
+    def send_data_to_milvus(self):
+        return None
+
+    def get_data_from_milvus(self):
+        return None
+
+    def clusterize_data(self):
+        return None
+
+
+def main():
+    test_obj = EmbeddingPipeline('test_md.xlsx')
+    print(test_obj.file_path)
+    test_obj.import_df()
+    print(test_obj.get_df())
+    test_obj.prepare_df()
+    print(test_obj.get_prepared_df())
+    test_obj.process_df()
+    print(test_obj.get_processed_data())
+
+
+main()
